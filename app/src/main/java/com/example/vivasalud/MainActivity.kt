17ccfb372555c9ebba8942eb4994d403ed904899
Jean-Huaringa <jeanhuaringach@gmail.com>
2025-06-27 02:20:06 -0500

    refactor: reorganizando codigo para mejorar la estructura

--- app/src/main/java/com/example/vivasalud/MainActivity.kt
+++ app/src/main/java/com/example/vivasalud/MainActivity.kt
@@ -8,7 +8,9 @@ import android.widget.AutoCompleteTextView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var spTypeDocument: AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,9 +23,15 @@ class MainActivity : AppCompatActivity() {
         spTypeDocument.setAdapter(adapter)
 
         val btnCreateAccount = findViewById<TextView>(R.id.btnCreateAccount)
+        val btnSiguiente = findViewById<TextView>(R.id.btnEnviar)
 
         btnCreateAccount.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSiguiente.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
     }
